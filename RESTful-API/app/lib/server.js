5da5b73041c77377a04bdabb44e1abebc8cf676b
Eldest Pasirula <aknutman@gmail.com>
2022-03-30 21:46:33 +0700

    restful api finished

--- RESTful-API/app/lib/server.js
+++ RESTful-API/app/lib/server.js
@@ -12,12 +12,14 @@ var fs = require('fs');
 var handlers = require('./handlers');
 const helpers = require('./helpers');
 var path = require('path');
+var util = require('util');
+var debug = util.debuglog('server');
 
 // Instantiate the server object
 var server = {};
 
 // Instantiating the HTTP server
-server.httpServer = http.createServer(function(req, res) {
+server.httpServer = http.createServer(function (req, res) {
     server.unifiedServer(req, res);
 });
 
@@ -26,12 +28,12 @@ server.httpsServerOptions = {
     'key': fs.readFileSync(path.join(__dirname, '/../https/key.pem')),
     'cert': fs.readFileSync(path.join(__dirname, '/../https/cert.pem'))
 };
-server.httpsServer = https.createServer(server.httpsServerOptions, function(req, res) {
+server.httpsServer = https.createServer(server.httpsServerOptions, function (req, res) {
     server.unifiedServer(req, res);
 });
 
 // All the server logic for both the http and https server
-server.unifiedServer = function(req, res) {
+server.unifiedServer = function (req, res) {
     // Get the URL and parse it
     var parsedUrl = url.parse(req.url, true);
 
@@ -51,15 +53,15 @@ server.unifiedServer = function(req, res) {
     // Get the payload, if any
     var decoder = new StringDecoder('utf-8');
     var buffer = '';
-    req.on('data', function(data) {
+    req.on('data', function (data) {
         buffer += decoder.write(data);
     });
-    req.on('end', function() {
+    req.on('end', function () {
         buffer += decoder.end();
 
         // Chhose the handler this request shoild go to. 
         // If one is not found, use the notFound handler
-        var chosenHandler = typeof(server.router[trimmedPath]) !== 'undefined' ?
+        var chosenHandler = typeof (server.router[trimmedPath]) !== 'undefined' ?
             server.router[trimmedPath] :
             handlers.notFound;
 
@@ -73,14 +75,14 @@ server.unifiedServer = function(req, res) {
         };
 
         // Route the request to the handler specified in the router
-        chosenHandler(data, function(statusCode, payload) {
+        chosenHandler(data, function (statusCode, payload) {
             // Use the status code called back by the handler, or default to 200
-            statusCode = typeof(statusCode) == 'number' ?
+            statusCode = typeof (statusCode) == 'number' ?
                 statusCode :
                 200;
 
             // Use the payload called back by the handler, or default to an empty object
-            payload = typeof(payload) == 'object' ?
+            payload = typeof (payload) == 'object' ?
                 payload : {};
 
             // Convert the payload to a string
@@ -91,8 +93,13 @@ server.unifiedServer = function(req, res) {
             res.writeHead(statusCode);
             res.end(payloadString);
 
-            // Log the request
-            console.log('Returning this response: ', statusCode, payloadString);
+            // If the response is 200, print green otherwise print red
+            if (statusCode == 200) {
+                debug('\x1b[32m%s\x1b[0m', method.toUpperCase() + ' /' + trimmedPath + ' ' + statusCode);
+            } else {
+                debug('\x1b[31m%s\x1b[0m', method.toUpperCase() + ' /' + trimmedPath + ' ' + statusCode);
+            }
+            debug('Returning this response: ', statusCode, payloadString);
         });
     });
 }
@@ -108,15 +115,15 @@ server.router = {
 };
 
 // Init script
-server.init = function() {
+server.init = function () {
     // Start the HTTP server
-    server.httpServer.listen(config.httpPort, function() {
-        console.log('The server is listening on port ' + config.httpPort);
+    server.httpServer.listen(config.httpPort, function () {
+        console.log('\x1b[36m%s\x1b[0m', 'The server is listening on port ' + config.httpPort);
     });
 
     // Start the HTTPS server
-    server.httpsServer.listen(config.httpsPort, function() {
-        console.log('The server is listening on port ' + config.httpsPort);
+    server.httpsServer.listen(config.httpsPort, function () {
+        console.log('\x1b[35m%s\x1b[0m', 'The server is listening on port ' + config.httpsPort);
     });
 };
 
