116a51dd6b10e7bead1549f82fcb40ee4488b45c
Eldest Pasirula <aknutman@gmail.com>
2022-04-09 04:40:12 +0700

    cli started

--- RESTful-API/app/index.js
+++ RESTful-API/app/index.js
@@ -6,17 +6,23 @@
 // Dependencies
 var server = require('./lib/server');
 var workers = require('./lib/workers');
+var cli = require('./lib/cli');
 
 // Declare the app
 var app = {};
 
 // Init function
-app.init = function() {
+app.init = function () {
     // Start the server
     server.init();
 
     // Start the worker
     workers.init();
+
+    // Start the CLI, but make sure it starts last
+    setTimeout(function () {
+        cli.init();
+    }, 50);
 };
 
 // Execute 
